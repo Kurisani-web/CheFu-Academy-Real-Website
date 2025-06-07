a01cb46411cec10bb8131625162a87af8b063985
Kurisani-web <kurisanim2@gmail.com>
2025-06-07 10:11:16 +0200

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,12 +6,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  apiKey: "",
+  authDomain: "cheforumreal.firebaseapp.com",
+  projectId: "cheforumreal",
+  storageBucket: "cheforumreal.appspot.com",
+  messagingSenderId: "441077080510",
+  appId: "1:441077080510:web:6df2e74dfdb214e17bd9ef",
+  measurementId: "G-HCCPEMB9D7"
 };
 
 // Initialize Firebase
