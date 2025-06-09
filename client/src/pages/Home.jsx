d6101fa1a016dfbe9505b3ae41d2e191eed2e8ec
Kurisani-web <kurisanim2@gmail.com>
2025-06-09 12:12:19 +0200

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,7 +8,9 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch('/api/post/getPosts');
+      const res = await fetch('https://chefu-academy-real-website.onrender.com/api/post/getPosts', {
+  credentials: 'include',
+});
       const data = await res.json();
       setPosts(data.posts);
     };
